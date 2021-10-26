a913bed0e65156343ab26346c0a33ab2f52f7b5f
Anton Voskanyan <voskanvl@gmail.com>
2021-10-27 01:52:27 +0300

    try vue

--- src/vue/app.js
+++ src/vue/app.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 
 /* eslint-disable no-new */
+// import Test from './components/test-component/test-component.vue';
+
 new Vue({
   el: '#vue-app',
+  template: '<div>HELLO from VUE</div>',
 });
---
+++ src/vue/components/test-component/test-component.vue
@@ -0,0 +1,11 @@
+<template>
+	<div id="test">
+		<h1>My Todo App!</h1>
+	</div>
+</template>
+
+<script src="test-component.js">
+</script>
+
+<style lang="less" src="test-component.less">
+</style>

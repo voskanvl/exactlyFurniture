13019eacc8b536fed7fa241509ea7fb8e014fbea
Anton Voskanyan <voskanvl@gmail.com>
2021-10-28 19:21:15 +0300

    close mobile

--- src/common.blocks/show-menu/show-menu.js
+++ src/common.blocks/show-menu/show-menu.js
@@ -1,5 +1,8 @@
 const showMenu = document.querySelector('.show-menu');
+const closeMenu = document.querySelector('.menu__close');
+
 const menu = document.querySelector('.header__menu');
+
 showMenu.addEventListener('click', () => {
   if (getComputedStyle(document.body).overflow === 'visible') {
     document.body.style.overflow = 'hidden';
@@ -9,3 +12,7 @@ showMenu.addEventListener('click', () => {
   menu.classList.toggle('header__menu_hide');
 });
 
+closeMenu.addEventListener('click', () => {
+  menu.classList.add('header__menu_hide');
+  document.body.style.overflow = 'visible';
+});

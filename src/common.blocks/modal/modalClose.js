54cd6bcc937fea03ac3a9a53b0e45fba7978b832
Anton Voskanyan <voskanvl@gmail.com>
2021-10-30 19:54:15 +0300

    through promise

--- src/common.blocks/modal/modalClose.js
+++ src/common.blocks/modal/modalClose.js
@@ -4,12 +4,22 @@ const modal = document.querySelector('.modal');
 const modalMain = document.querySelector('.modal__main');
 const modalButton = document.querySelector('.modal__close');
 
+const modalMainIsHide = (wait, period) => new Promise((resolve, reject) => {
+  let i = 0;
+  const interval = setInterval(() => {
+    i++;
+    if (!modalMain.classList.contains('modal__main-show')) {
+      clearInterval(interval);
+      resolve('ok');
+    }
+    if (period * i > wait) {
+      clearInterval(interval);
+      reject(new Error('main block of modal could not return to its original state'));
+    }
+  }, period);
+});
+
 modalButton.addEventListener('click', () => {
   modalMain.classList.remove('modal__main-show');
-  const interval = setInterval(() => {
-    if (!modalMain.classList.contains('modal__main-show')) {
-      show(modal, 'modal_show')();
-      clearInterval(interval);
-    }
-  }, 100);
+  modalMainIsHide(600, 100).then(show(modal, 'modal_show')).catch((error) => { throw error; });
 });

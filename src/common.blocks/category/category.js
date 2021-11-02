04ee0c8148c6983e442f21a08209682d84001f21
Anton Voskanyan <voskanvl@gmail.com>
2021-11-02 20:17:54 +0300

    mobile modal finish

--- src/common.blocks/category/category.js
+++ src/common.blocks/category/category.js
@@ -2,8 +2,9 @@ const categoryList = document.querySelector('.category__list');
 categoryList.addEventListener('click', (event) => {
   const selected = document.querySelector('.category__item[selected]');
   const selecteditem = event.target.closest('.category__item');
-  console.log('🚀 ~ selecteditem', selecteditem);
   if (selecteditem != null && selecteditem.tagName === 'LI') {
+    const value = event.target.textContent;
+    document.querySelector('.modal__title').textContent = value;
     selected.removeAttribute('selected');
     selecteditem.setAttribute('selected', 'selected');
   }

460f625db6fbfb1260f710dd3f2f0fe857b74125
Anton Voskanyan <voskanvl@gmail.com>
2021-11-02 10:21:08 +0300

    pre select

---
+++ src/common.blocks/select/select.js
@@ -0,0 +1,6 @@
+// const select = document.querySelector('.select > select');
+// const selectTitle = select.querySelector('optgroup');
+
+// select.addEventListener('change', (event) => {
+//   selectTitle.label = event.target.value;
+// });
